443cf96a8eb00bde5035bb7b13280b898ad8ffd7
Alexander Bogomolov <alexbogomolov.kharkiv@gmail.com>
2022-07-02 14:15:11 +0300

    print to stdout instead of progress bar

--- train.py
+++ train.py
@@ -238,7 +238,7 @@ def train(opt):
 
                     epoch_loss.append(float(loss))
 
-                    progress_bar.set_description(
+                    print(
                         'Step: {}. Epoch: {}/{}. Iteration: {}/{}. Cls loss: {:.5f}. Reg loss: {:.5f}. Total loss: {:.5f}'.format(
                             step, epoch, opt.num_epochs, iter + 1, num_iter_per_epoch, cls_loss.item(),
                             reg_loss.item(), loss.item()))
